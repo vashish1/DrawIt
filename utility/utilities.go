3c8166203a3835571713fc77b980492758330ea0
vashish1 <vashishtiv@gmail.com>
2020-05-04 22:31:58 +0530

    Minor changes

--- utility/utilities.go
+++ utility/utilities.go
@@ -2,11 +2,13 @@ package utility
 
 import (
 	"math/rand"
+	"time"
 
 	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 func GenerateColor() string {
-	c := colorful.Hsv(rand.Float64()*360.0, 0.8, 0.8)
+	rand.Seed(time.Now().UnixNano())
+	c := colorful.Hsv(rand.Float64()*360.0, 0.4, 0.8)
 	return c.Hex()
 }
