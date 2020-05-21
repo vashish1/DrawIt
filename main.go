1bc6308bd43d03aea899d019b27af8200e5c795c
vashish1 <vashishtiv@gmail.com>
2020-05-21 20:37:16 +0530

    changes

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"DrawIt/hub"
+	"DrawIt/user"
 	"log"
-  "net/http"
-  "DrawIt/hub"
+	"net/http"
 )
 
+// type m map[string]*hub.Hub
+
+
 func main() {
-	h := hub.NewHub()
-	go h.Run()
-	http.HandleFunc("/ws", h.HandleWebSocket)
+	
+var users = []user.User{
+	{
+		Name: "yashi",
+		Slot: hub.NewHub(),
+	},
+	{
+		Name: "Anni",
+		Slot: hub.NewHub(),
+	},
+}
+
+	// var users=&user.Users
+	// for _, u := range users {
+	// 	u.Slot = hub.NewHub()
+	// 	go u.Slot.Run()
+		 
+	// }
+	h1:=users[0].Slot
+	h2:=users[1].Slot
+	// h := hub.NewHub()
+	go h1.Run()
+	go h2.Run()
+	
+	http.HandleFunc("/yashi",h1.HandleWebSocket)
+	http.HandleFunc("/anni",h2.HandleWebSocket)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
---
+++ user/user.go
@@ -0,0 +1,10 @@
+package user
+
+import "DrawIt/hub"
+
+type User struct {
+	Name string
+	Slot *hub.Hub
+}
+
+// var users []User
